418402b42c478c3beeab458c6025f3d74cdf13c0
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-07 09:09:47 +0530

    usecase1

--- src/bridgelabz/TicTacToeGame.java
+++ src/bridgelabz/TicTacToeGame.java
@@ -1,10 +1,32 @@
 package bridgelabz;
-import java.util.Scanner;
+//import java.util.Scanner;
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     
+        char[] a = new char[10];
+        //a[0]=0;
+        a[1] = ' ';
+        a[2] = ' ';
+        a[3] = ' ';
+        a[4] = ' ';
+        a[5] = ' ';
+        a[6] = ' ';
+        a[7] = ' ';
+        a[8] = ' ';
+        a[9] = ' ';
+        
+        display(a);
+	}
+	
+	public static void display(char[] a) {
+		
+		for(int i = 0; i < a.length; i++) {
+				System.out.print(a[i]+" ");
+			}
+			System.out.println();
+			
+		}
 	}
 
-}
+
